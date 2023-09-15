4e6d6ede5bfb6fd99b6ce2a7b395bffb5ca41492
Marcela Kramer <101991467+marcelakramer@users.noreply.github.com>
2023-09-15 15:23:44 -0300

    Update pessoa-fisica.ts

--- app/ts/model/pessoa-fisica.ts
+++ app/ts/model/pessoa-fisica.ts
@@ -1,6 +1,6 @@
 class PessoaFisica extends Pessoa {
 
-    private _cpf : string;
+    readonly _cpf : string;
 
     constructor(nome: string, idade: number, dataNascimento: Date, cpf: string) {
         super(nome, idade, dataNascimento);
